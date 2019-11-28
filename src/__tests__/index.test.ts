b4e5264e06fb7536308919dfdfb838549d355c88
Ian Schmitz <ianschmitz@gmail.com>
2019-11-27 21:54:28 -0800

    Cleanup tests

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,14 +1,22 @@
-
-const JSDomEnvironment = jest.requireActual("../index");
+import JSDomEnvironment from "../";
 
 describe("JSDomEnvironment", () => {
+  let instance: JSDomEnvironment | undefined;
+
+  afterEach(async () => {
+    if (instance) {
+      await instance.teardown();
+      instance = undefined;
+    }
+  });
+
   it("should configure setTimeout/setInterval to use the browser api", () => {
-    const env1 = new JSDomEnvironment({});
+    instance = new JSDomEnvironment({} as any);
 
-    env1.fakeTimers.useFakeTimers();
+    instance.fakeTimers!.useFakeTimers();
 
-    const timer1 = env1.global.setTimeout(() => {}, 0);
-    const timer2 = env1.global.setInterval(() => {}, 0);
+    const timer1 = instance.global.setTimeout(() => {}, 0);
+    const timer2 = instance.global.setInterval(() => {}, 0);
 
     [timer1, timer2].forEach(timer => {
       expect(typeof timer).toBe("number");
